be66f4498d2db41d5aec5a7bcbf499d20bedeb51
ABIN VARGHEXE <abinvarghese092005@gmail.com>
2024-07-21 23:45:27 +0530

    Update banner and explore menu styles

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import "./Banner.css";
 
 const Banner = () => {
   return (
     <div>
-      <h1>
-        <span>{`It’s Now `}</span>
-        <span className="word-colour-1">More Easy</span>
-        <span>{` to `}</span>
-        <span className="word-colour-2">Order</span>
-        <span>{` by Our Mobile `}</span>
-        <span className="word-colour-1">App</span>
-      </h1>
-      <p>
-        All you need to do is downlode one of the best delivery apps, make a and
-        most companies are opting for mobile app devlopment for food delivery
-      </p>
+      <div className="maincontiner">
+        <div className="textcontiner-banner">
+          <h1>
+            It’s Now <span className="word-colour-1">More Easy</span> to{" "}
+            <span className="word-colour-2">Order</span> by Our Mobile <span className="word-colour-1">App</span> 
+          </h1>
+          <p className="desc-banner">
+            All you need to do is downlode one of the best delivery apps, make a
+            and most companies are opting for mobile app devlopment for food
+            delivery
+          </p>
+        
+        </div>
+      </div>
     </div>
   );
 };
